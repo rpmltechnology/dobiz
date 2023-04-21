c0510292c9db9a2b7e61b99847f46645e023143c
sundranigoldy <sundranigoldy@gmail.com>
2023-04-21 20:13:56 +0530

    Dashboard,Card,Share functionality,blog,comment,search,product Updated

--- DOBIZINIDA/settings.py
+++ DOBIZINIDA/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='goldygo56@gmail.com'
-EMAIL_HOST_PASSWORD = 'gzngdwkdmhfzqhus'
+EMAIL_HOST_USER ='your key'
+EMAIL_HOST_PASSWORD = 'your secret'
 
 
 ####################
